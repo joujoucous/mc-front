936b12b88797336eea4121ae84fc0e1e4f4b831e
jul <coustenoble.julie@gmail.com>
2021-01-19 16:10:39 +0100

    initial front commit

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-const Home = () => (
+const Login = () => (
   <div>
-    <h1>Homepage</h1>
-    <Link href="/login">
-      <a>Go to login</a>
+    <h1>Login</h1>
+    <Link href="/">
+      <a>Back to home</a>
     </Link>
     <Link href="/sign-in">
       <a>Go to sign in</a>
     </Link>
   </div>
 );
-export default Home;
+export default Login;
