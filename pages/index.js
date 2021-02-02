e5918042ebf567ee70f6ba3feb5c47bf1a380e90
Axel <axel.buee@epita.fr>
2021-02-02 14:04:02 +0100

    commit conflict css

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
-
+import Button from 'react-bootstrap/Button'
+import Image from 'next/image'
+//<Image class="homeImage" src="/images/image.jpg" alt="me" layout="fill"/>
+//<Image class="homeImage" src="/images/image.jpg" alt="me" layout="fill" />
 const Home = () => (
   <div>
-    <h1>Homepage</h1>
-    <Link href="/login">
-      <a>Go to login</a>
-    </Link>
-    <Link href="/sign-in">
-      <a>Go to sign in</a>
-    </Link>
+    <div class="content-center my-auto">
+      <div class="container">
+        <div class="title-brand">
+          <h1 class="presentation-title">Paper Kit React</h1>
+          
+        </div>
+      </div>
+    </div>
+    <div class="imageContainer">
+            <img class="homeImage" src="/images/image.jpg"/>
+            <img class="homeImage" src="/images/image.jpg"/>
+    </div>
   </div>
 );
 export default Home;
