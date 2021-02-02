cae5da6afdabc3ffbb8172b9a7d69a003fb2b2a7
Axel <axel.buee@epita.fr>
2021-02-02 14:31:23 +0100

    push before merge

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,12 @@ import Image from 'next/image'
 //<Image class="homeImage" src="/images/image.jpg" alt="me" layout="fill"/>
 //<Image class="homeImage" src="/images/image.jpg" alt="me" layout="fill" />
 const Home = () => (
-  <div>
+  <div style={{backgroundColor: 'black'}}>
     <div class="content-center my-auto">
-      <div class="container">
         <div class="title-brand">
-          <h1 class="presentation-title">Paper Kit React</h1>
+          <h1 class="presentation-title">Does it Exist ?</h1>
           
         </div>
-      </div>
     </div>
     <div class="imageContainer">
             <img class="homeImage" src="/images/image.jpg"/>
